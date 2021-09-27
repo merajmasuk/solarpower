ed232c9ea27beb72c6b3d2d1b632ea39bd3636e6
Meraj al Maksud <merajmasuk@gmail.com>
2021-09-27 13:37:39 +0600

    Project setup

---
+++ src/main/java/com/nasa/solarpower/SolarpowerApplication.java
@@ -0,0 +1,13 @@
+package com.nasa.solarpower;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SolarpowerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SolarpowerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nasa/solarpower/SolarpowerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nasa.solarpower;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SolarpowerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
